d86780b91b9b6cf61788b9f9e41f0a5d8a588737
eh158 <edward.hsieh2@duke.edu>
2019-02-20 21:07:50 -0500

    edited Tester class

---
+++ src/CA/SimulationTest.java
@@ -0,0 +1,19 @@
+package CA;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SimulationTest {
+
+    @BeforeEach
+    public void setUp(){
+
+    }
+
+    @Test
+    public void testSetCurrentToNextState(){
+        
+    }
+}
